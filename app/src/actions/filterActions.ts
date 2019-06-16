28620b2fc1e30b77abb53537ff8a4196bba5fdf5
Jander <andresherrerof@gmail.com>
2019-06-16 22:29:25 +0200

    Removed unused types

--- app/src/actions/filterActions.ts
+++ app/src/actions/filterActions.ts
@@ -1,18 +1,3 @@
-import { DataType } from "../mokedData/types";
-import * as R from "ramda";
-
-type DataInput = {
-  field: string;
-  index: number;
-  value: number | boolean | undefined;
-  rows: Array<DataType>;
-};
-
-type SelectedRowDataInput = {
-  keys: Array<number | string>;
-  rows: Array<DataType>;
-};
-
 export const filterItems = (
   activated: boolean,
   type: "availability" | "confirmed"
